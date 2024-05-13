aca7ffdb77114e8716b710f84f697351e68a0f72
ubitinaitis <bitinaitis.u@northeastern.edu>
2024-05-13 18:04:07 -0400

    final touches

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -3,10 +3,10 @@ export default function CourseStatus() {
       <div id="wd-course-status">
         <h2>Course Status</h2>
         <button>Unpublish</button> <button>Publish</button>
-        <button>Import Existing Content</button> <button>Import From Commons</button>
-        <button>Choose Home Page</button> <button>View Course Stream</button>
-        <button>New Announcement</button> <button>New Analytics</button>
-        <button>View Course Notifications</button>
+        <button style={{display: "block"}}>Import Existing Content</button> <button style={{display: "block"}}>Import From Commons</button>
+        <button style={{display: "block"}}>Choose Home Page</button> <button style={{display: "block"}}>View Course Stream</button>
+        <button style={{display: "block"}}>New Announcement</button> <button style={{display: "block"}}>New Analytics</button>
+        <button style={{display: "block"}}>View Course Notifications</button>
 
         <button>View Course Notifications</button>
       </div>
--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,30 +1,68 @@
 export default function Modules() {
-    return (
-      <div>
-        {/* Collapse All button, View Progress button, etc. */}
-        <ul id="wd-modules">
-          <li className="wd-module">
-            <div className="wd-title">Week 1</div>
-            <ul className="wd-lessons">
-              <li className="wd-lesson">
-                <span className="wd-title">LEARNING OBJECTIVES</span>
-                <ul className="wd-content">
-                  <li className="wd-content-item">Introduction to the course</li>
-                  <li className="wd-content-item">Learn what is Web Development</li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-          <li className="wd-module">
-            <div className="wd-title">Week 2</div>
-            <ul className="wd-lessons">
-              <li className="wd-lesson">
-                <span className="wd-title">LEARNING OBJECTIVES</span>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-  );}
-  
-  
+  return (
+    <div>
+      <button id="wd-button" type="button">
+        Collapse All
+      </button>
+      <button id="wd-button" type="button">
+        View Progress
+      </button>
+      <select id="wd-select-one-genre">
+        <option selected value="Publish All">
+          Publish All
+        </option>
+      </select>
+      <button id="wd-button" type="button">
+        + Module
+      </button>
+      <ul id="wd-modules">
+        <li className="wd-module">
+          <div className="wd-title">
+            Week 1, Lecture 1 - Course Introduction, Syllabus, Agenda
+          </div>
+          <ul className="wd-lessons">
+            <li className="wd-lesson">
+              <span className="wd-title">LEARNING OBJECTIVES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">Introduction to the course</li>
+                <li className="wd-content-item">
+                  Learn what is Web Development
+                </li>
+              </ul>
+            </li>
+            <li className="wd-lesson">
+              <span className="wd-title">READING</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">
+                  Full Stack Developer - Chapter 1 - Introduction
+                </li>
+                <li className="wd-content-item">
+                  Full Stack Developer - Chapter 2 - Creating User Interfaces
+                  With HTML
+                </li>
+              </ul>
+            </li>
+            <li className="wd-lesson">
+              <span className="wd-title">SLIDES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">
+                  Introduction to Web Development
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li className="wd-module">
+          <div className="wd-title">
+            Week 1, Lecture 2 - Formatting User Interfaces with HTML
+          </div>
+          <ul className="wd-lessons">
+            <li className="wd-lesson">
+              <span className="wd-title">LEARNING OBJECTIVES... etc.</span>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  );
+}
